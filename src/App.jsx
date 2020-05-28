66703cea9933bb5edd67fe94741bde3e87e8b645
Edward Hong <edward@Edwards-MacBook-Air.local>
2020-05-28 23:03:03 +1200

    3.3 Composing Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-class HelloWorld extends React.Component {
+class IssueFilter extends React.Component {
   render() {
-    const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe']
-    const helloContinents = Array.from(continents, (c) => `Hello ${c}!`)
-    const message = helloContinents.join(' ')
+    return <div>This is a placeholder for the issue filter.</div>
+  }
+}
 
+class IssueTable extends React.Component {
+  render() {
+    return <div>This is a placeholder a table of issues.</div>
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return <div>This is a placeholder for a form to add an issue.</div>
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
     return (
-      <div title="Outer Div">
-        <h1>{message}</h1>
-      </div>
+      <>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </>
     )
   }
 }
 
-const element = <HelloWorld />
+const element = <IssueList />
 
 ReactDOM.render(element, document.getElementById('content'))
