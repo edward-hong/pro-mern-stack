6ff6edd86ffc0aaad829f429cc6f562b445ced6f
Edward Hong <edward.hong527@gmail.com>
2020-06-22 18:42:15 +1200

    4.1 Initial State

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const issues = [
+const initialIssues = [
   {
     id: 1,
     status: 'New',
@@ -43,8 +43,12 @@ class IssueRow extends React.Component {
 }
 
 class IssueTable extends React.Component {
+  constructor() {
+    super()
+    this.state = { issues: initialIssues }
+  }
   render() {
-    const issueRows = issues.map((issue) => (
+    const issueRows = this.state.issues.map((issue) => (
       <IssueRow key={issue.id} issue={issue} />
     ))
     return (
