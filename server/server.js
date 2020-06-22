d89f36a3e65b52f90abe912c9939ddfad27ba9cf
Edward Hong <edward.hong527@gmail.com>
2020-06-23 00:30:55 +1200

    5.6 The List API

--- server/server.js
+++ server/server.js
@@ -4,13 +4,39 @@ const { ApolloServer } = require('apollo-server-express')
 
 let aboutMessage = 'Issue Tracker API v1.0'
 
+const issueDB = [
+  {
+    id: 1,
+    status: 'New',
+    owner: 'Ravan',
+    effort: 5,
+    created: new Date('2018-08-15'),
+    due: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    effort: 14,
+    created: new Date('2018-08-16'),
+    due: new Date('2018-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+]
+
 function setAboutMessage(_, { message }) {
   return (aboutMessage = message)
+}
+
+function issueList() {
+  return issueDB
 }
 
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setAboutMessage,
