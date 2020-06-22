c1d1fc05cb4a73cb02bcd4bc61149178a1caf4b5
Edward Hong <edward.hong527@gmail.com>
2020-06-23 03:03:07 +1200

    5.12 Input Validations

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const { GraphQLScalarType } = require('graphql')
 const { Kind } = require('graphql/language')
-const { ApolloServer } = require('apollo-server-express')
+const { ApolloServer, UserInputError } = require('apollo-server-express')
 
 let aboutMessage = 'Issue Tracker API v1.0'
 
@@ -32,11 +32,24 @@ function setAboutMessage(_, { message }) {
 }
 
 function issueAdd(_, { issue }) {
+  validateIssue(issue)
   issue.created = new Date()
   issue.id = issueDB.length + 1
-  if (issue.status === undefined) issue.status = 'New'
   issueDB.push(issue)
   return issue
+}
+
+function validateIssue(issue) {
+  const errors = []
+  if (issue.title.length < 3) {
+    errors.push('Field "title" must be at least 3 characters long')
+  }
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is required when status is "Assigned"')
+  }
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors })
+  }
 }
 
 function issueList() {
@@ -50,10 +63,14 @@ const GraphQLDate = new GraphQLScalarType({
     return value.toISOString()
   },
   parseValue(value) {
-    return new Date(value)
+    const dateValue = new Date(value)
+    return isNaN(dateValue) ? undefined : dateValue
   },
   parseLiteral(ast) {
-    return ast.kind === Kind.STRING ? new Date(ast.value) : undefined
+    if (ast.kind === Kind.STRING) {
+      const value = new Date(ast.value)
+      return isNaN(value) ? undefined : value
+    }
   },
 })
 
@@ -72,6 +89,10 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
   resolvers,
+  formatError: (error) => {
+    console.log(error)
+    return error
+  },
 })
 
 const app = express()
