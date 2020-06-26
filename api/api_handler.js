54a5a06eb705db0c246806e4d0829f872d94dd80
Edward Hong <edward.hong527@gmail.com>
2020-06-26 13:10:10 +1200

    8.1 Back-End Modules

---
+++ api/api_handler.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const fs = require('fs')
+const { ApolloServer } = require('apollo-server-express')
+
+const GraphQLDate = require('./graphql_date')
+const { setMessage, getMessage } = require('./about')
+const { list, add } = require('./issue')
+
+const resolvers = {
+  Query: {
+    about: getMessage,
+    issueList: list,
+  },
+  Mutation: {
+    setAboutMessage: setMessage,
+    issueAdd: add,
+  },
+  GraphQLDate,
+}
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
+  resolvers,
+  formatError: (error) => {
+    console.log(error)
+    return error
+  },
+})
+
+function installHandler(app) {
+  const enableCors = (process.env.ENABLE_CORS || true) === 'true'
+  console.log('CORS setting:', enableCors)
+  server.applyMiddleware({ app, path: '/graphql', cors: enableCors })
+}
+
+module.exports = { installHandler }
